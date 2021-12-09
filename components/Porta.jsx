ca4108a15dac030862b0d489bef2aea9c609366c
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-12-09 19:38:08 -0300

    Porta parte#1

---
+++ components/Porta.jsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Porta.module.css"
+
+export default function Porta() {
+    return (
+        <div className={styles.porta}>
+            <div className={styles.numeroPorta}>3</div>
+        </div>
+    )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Porta from '../components/Porta'
 import Presente from '../components/Presente'
 
 
 export default function Home() {
   return (
     <div>
-      <Presente/>
+      <Porta />
     </div>
   )
 }
