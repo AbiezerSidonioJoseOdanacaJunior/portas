5ea814e8e31830246742039475d6f8bd0d3a6ba0
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-12-09 20:55:52 -0300

    Base da porta

--- components/Porta.jsx
+++ components/Porta.jsx
@@ -8,6 +8,7 @@ export default function Porta(props) {
                     <div className={styles.numeroPorta}>3</div>
                 </div>
            </div>
+           <div className={styles.chao}></div>
        </div>
     )
 }
