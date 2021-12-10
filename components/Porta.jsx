3a21e11b46ad4406ac391dfb0203501178ebc36a
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-12-09 21:07:19 -0300

    Criando mceneta da porta

--- components/Porta.jsx
+++ components/Porta.jsx
@@ -6,6 +6,7 @@ export default function Porta(props) {
            <div className={styles.estrutura}>
                 <div className={styles.porta}>
                     <div className={styles.numeroPorta}>3</div>
+                    <div className={styles.maceneta}></div>
                 </div>
            </div>
            <div className={styles.chao}></div>
