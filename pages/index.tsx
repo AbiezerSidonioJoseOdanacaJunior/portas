16060a2ae558a3cecb63582ec693728aec890566
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-12-15 10:17:39 -0300

    Finalizando Formulario

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,15 @@ import Presente from '../components/Presente'
 import criarPorta, { atualizarPortas } from '../funtions /portas'
 import PortaModel from '../model/porta'
 import Link from 'next/link'
+import EntradaNumerica from '../components/EntradaNumerica'
 
 
 
 export default function Formulario() {
+
+  const [qtdPortas, setQtdPortas ] = useState(3)
+  const [comPresente, setComPresente ] = useState(1)
+
 
   return (
     <div className={styles.formulario}>
@@ -19,12 +24,20 @@ export default function Formulario() {
      <Cartao bgcolor='#c0392c'>
      <h1>Monty Hall</h1>
      </Cartao>
-     <Cartao></Cartao>
+     <Cartao>
+       <EntradaNumerica text='Qtd Portas?' 
+       value={qtdPortas} 
+       onChange={novaQtd => setQtdPortas(novaQtd) } />
+     </Cartao>
      </div>
      <div>
-     <Cartao ></Cartao>
+     <Cartao >
+     <EntradaNumerica text='Porta com Presente?' 
+       value={comPresente} 
+       onChange={novaPortaCompresente => setComPresente(novaPortaCompresente) } />
+     </Cartao>
      <Cartao bgcolor='#28a085'>
-       <Link href={`/jogo/4/2`}>
+       <Link href={`/jogo/${qtdPortas}/${comPresente}`}>
        <h2 className={styles.link}>Iniciar!</h2>
        </Link>
      
